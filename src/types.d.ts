aef6e26b212fcf0f701e176688581d8dd0665392
John Wakley <john.wakley@icloud.com>
2025-01-09 16:07:12 -0500

    added types and threejs

---
+++ src/types.d.ts
@@ -0,0 +1,35 @@
+declare type KeyValue = { [key: string]: any };
+
+type JSONValue =
+    | string
+    | number
+    | boolean
+    | null
+    | JSONValue[]
+    | { [key: string]: JSONValue };
+
+declare type DataJson = {
+    [k: string]: JSONValue,
+};
+
+// interface JSONObject {
+//     [k: string]: JSONValue
+// }
+
+// interface JSONArray extends Array<JSONValue> { }
+
+declare module "*.glsl" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.vert" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.frag" {
+    const value: string;
+    export default value;
+}
+
